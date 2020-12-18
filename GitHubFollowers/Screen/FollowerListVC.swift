1dc4f35936d6e6d96ac2036288a53ed400347c9f
AlekseyK <aleksey.kabishau@gmail.com>
2020-12-17 23:14:09 -0500

    refactor: replace optional values in completion handler with Result type

--- GitHubFollowers/Screen/FollowerListVC.swift
+++ GitHubFollowers/Screen/FollowerListVC.swift
@@ -10,19 +10,20 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
-            print("Followers.count = \(followers.count)")
-            print(followers)
         }
     }
     
